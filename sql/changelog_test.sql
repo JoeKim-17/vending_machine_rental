4464d6c3576eea12fc571107627366c02b5fe2f3
Joseph Kimathi <joseph.kimathi@bbd.co.za>
2024-02-15 20:42:54 +0200

    delete test table

--- sql/changelog_test.sql
+++ sql/changelog_test.sql
@@ -1,7 +1,1 @@
-create table testTable(
-	ID int identity(1,1) not null,
-	personName nvarchar(400) null,
-	personSurname nvarchar(400) not null,
-	cost money null,
-	constraint testPK primary key (ID)
-);
+drop table testTable;

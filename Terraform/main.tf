c0e7f15ae97b6d45939318d49f9166cd24e80126
Kat <Katlego.Kungoane@bbd.co.za>
2024-02-16 10:10:44 +0200

    Teraform: fixed ports

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,7 +8,7 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.5.2"
 
-  name                 = "vending"
+  name                 = "renting"
   cidr                 = "10.0.0.0/16"
   azs                  = data.aws_availability_zones.available.names
   # azs                  = ["eu-west-1"]
@@ -17,54 +17,54 @@ module "vpc" {
   enable_dns_support   = true
 }
 
-resource "aws_db_subnet_group" "vending" {
-  name       = "vending"
+resource "aws_db_subnet_group" "renting" {
+  name       = "renting"
   subnet_ids = module.vpc.public_subnets
 
   tags = {
-    Name = "vending"
+    Name = "renting"
   }
 }
 
 resource "aws_security_group" "rds" {
-  name   = "vending_rds"
+  name   = "renting_rds"
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port   = 5432
-    to_port     = 5432
+    from_port   = 1433
+    to_port     = 1433
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 5432
-    to_port     = 5432
+    from_port   = 0
+    to_port     = 0
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "vending_rds"
+    Name = "renting_rds"
   }
 }
 
-resource "aws_db_parameter_group" "vending" {
-  name   = "vending"
+resource "aws_db_parameter_group" "renting" {
+  name   = "renting"
   family = "sqlserver-ex-15.0"
 }
 
-resource "aws_db_instance" "vending" {
-  identifier             = "vending"
+resource "aws_db_instance" "renting" {
+  identifier             = "renting"
   instance_class         = "db.t3.micro"
   allocated_storage      = 20
   engine                 = "sqlserver-ex"
   engine_version         = "15.00.4345.5.v1"
   username               = "edu"
   password               = var.db_password
-  db_subnet_group_name   = aws_db_subnet_group.vending.name
+  db_subnet_group_name   = aws_db_subnet_group.renting.name
   vpc_security_group_ids = [aws_security_group.rds.id]
-  parameter_group_name   = aws_db_parameter_group.vending.name
+  parameter_group_name   = aws_db_parameter_group.renting.name
   publicly_accessible    = true
   skip_final_snapshot    = true
 }
--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -3,19 +3,19 @@
 
 output "rds_hostname" {
   description = "RDS instance hostname"
-  value       = aws_db_instance.vending.address
+  value       = aws_db_instance.renting.address
   sensitive   = false
 }
 
 output "rds_port" {
   description = "RDS instance port"
-  value       = aws_db_instance.vending.port
+  value       = aws_db_instance.renting.port
   sensitive   = false
 }
 
 output "rds_username" {
   description = "RDS instance root username"
-  value       = aws_db_instance.vending.username
+  value       = aws_db_instance.renting.username
   sensitive   = false
 }
 
